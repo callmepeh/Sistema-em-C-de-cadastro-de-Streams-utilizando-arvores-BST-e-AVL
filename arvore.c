0b5881186262553522571443b18575efc841735c
callmepeh <pedrosasada@gmail.com>
2025-08-23 15:01:20 -0300

    new Arq

---
+++ arvore.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "arvore.h"
+
+No* criarNo(TipoDado tipo, DadoUnion dado) {
+    No* novo = (No*) malloc(sizeof(No));
+    novo->tipo = tipo;
+    novo->dado = dado;
+    novo->esq = novo->dir = NULL;
+    return novo;
+}
---
+++ arvore.h
@@ -0,0 +1,41 @@
+#ifndef ARVORE_H
+#define ARVORE_H
+
+typedef enum {STREAM, PROGRAMA} TipoDado;
+typedef enum {DIARIO, SEMANAL, QUINZENAL, MENSAL} Periodicidade;
+typedef enum {AO_VIVO, SOB_DEMANDA} TipoTransmissao;
+
+
+typedef struct {
+    char NomeStream[50];
+    char Site[100];
+    Categorias *categorias
+} Stream;
+
+typedef struct{
+    char NomePrograma[50];
+    char NomeApresentador[50];
+    Periodicidade periodicidade;
+    int Tempo;
+    char HorarioInicio[10];
+    TipoTransmissao  tipotransmissao;
+} Programa;
+
+typedef union {
+    Stream stream;
+    Programa programa;
+} DadoUnion;
+
+typedef struct No{
+    TipoDado tipo;
+    DadoUnion dado;
+    No *esq, *dir;
+}No; 
+
+
+Stream* criarStream(char* nome, char* site);
+Stream* inserirStream(Stream* raiz, Stream* nova);
+Stream* buscarStream(Stream* raiz, char* nome);
+void mostrarStreams(Stream* raiz); 
+
+#endif
